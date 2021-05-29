676bbe6228daef4e012291fea0c159fd4e85dad5
gregjukes <83824936+gregjukes@users.noreply.github.com>
2021-05-28 23:07:31 -0400

    create toggle interaction in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Welcome to my UX design portfolio site.");
+console.log("Welcome to my UX design portfolio site."); //trial welcome message
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
